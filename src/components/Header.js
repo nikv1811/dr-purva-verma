fcb03873b81522db1ebda069fcaf25be5ea607d3
Nikhil Verma <nikhilverma@Mac.lan>
2026-07-20 11:40:03 +0530

    faq

--- src/components/Header.js
+++ src/components/Header.js
@@ -176,6 +176,7 @@ const Header = () => {
                     <a href="#home" className="text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Home</a>
                     <a href="#about" className="text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>About</a>
                     <a href="#services" className="text-gray-600 hover:text-blue-600 transition duration-300">Services</a> {/* Added Services link */}
+                    <a href="#faq" className="text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>FAQ</a>
                     <a href="#contact" className="text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Contact</a>
                     <a href="#order-medicine" className="text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Order Medicine</a>
                 </nav>
@@ -197,6 +198,7 @@ const Header = () => {
                         <a href="#home" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Home</a>
                         <a href="#about" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>About</a>
                         <a href="#services" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Services</a> {/* Added Services link */}
+                        <a href="#faq" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>FAQ</a>
                         <a href="#contact" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Contact</a>
                         <a href="#order-medicine" className="block text-gray-600 hover:text-blue-600 transition duration-300" onClick={closeMenu}>Order Medicine</a>
                     </div>
